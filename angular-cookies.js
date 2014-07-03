dac89d61f3f1d0043ebe4acfa6078cebf56d9f87
jenkins <ci-bot@angularjs.org>
2014-07-03 01:56:08 -0700

    v1.3.0-build.2873+sha.2f34950

--- angular-cookies.js
+++ angular-cookies.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.20-build.273+sha.2fd8dc7
+ * @license AngularJS v1.3.0-build.2873+sha.2f34950
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -142,7 +142,7 @@ angular.module('ngCookies', ['ng']).
    * @example
    *
    * ```js
-   * function ExampleController($cookies) {
+   * function ExampleController($cookieStore) {
    *   // Put cookie
    *   $cookieStore.put('myFavorite','oatmeal');
    *   // Get cookie
