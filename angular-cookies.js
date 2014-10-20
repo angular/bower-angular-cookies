de557fbb2c65a384f2a3c9be079ff73427a1199c
jenkins <ci-bot@angularjs.org>
2014-10-20 02:37:00 -0700

    v1.3.1-build.3457+sha.ba9d073

--- angular-cookies.js
+++ angular-cookies.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.27-build.523+sha.a56435f
+ * @license AngularJS v1.3.1-build.3457+sha.ba9d073
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -95,7 +95,7 @@ angular.module('ngCookies', ['ng']).
         }
 
         //update all cookies updated in $cookies
-        for(name in cookies) {
+        for (name in cookies) {
           value = cookies[name];
           if (!angular.isString(value)) {
             value = '' + value;
