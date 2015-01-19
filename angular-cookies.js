827dfba715cfba7fa98a2ce8b413769152ce90cd
jenkins <ci-bot@angularjs.org>
2015-01-19 07:04:39 -0800

    v1.4.0-build.3797+sha.0db5b21

--- angular-cookies.js
+++ angular-cookies.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.3.10-build.25+sha.9b8df52
- * (c) 2010-2014 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.4.0-build.3797+sha.0db5b21
+ * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
@@ -165,7 +165,7 @@ angular.module('ngCookies', ['ng']).
          * Returns the value of given cookie key
          *
          * @param {string} key Id to use for lookup.
-         * @returns {Object} Deserialized cookie value.
+         * @returns {Object} Deserialized cookie value, undefined if the cookie does not exist.
          */
         get: function(key) {
           var value = $cookies[key];
