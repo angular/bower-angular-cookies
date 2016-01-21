cf2e23660fbbc0d9e74eaa8a535b42ae58b70805
jenkins <ci-bot@angularjs.org>
2016-01-21 05:21:45 -0800

    v1.4.9

--- angular-cookies.js
+++ angular-cookies.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.5.0-build.4525+sha.2cb1989
- * (c) 2010-2016 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.4.9
+ * (c) 2010-2015 Google, Inc. http://angularjs.org
  * License: MIT
  */
 (function(window, angular, undefined) {'use strict';
