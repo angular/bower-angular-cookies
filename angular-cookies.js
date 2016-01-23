0312abe2269b472f10796c9d93a70000779f1e94
jenkins <ci-bot@angularjs.org>
2016-01-23 05:25:09 -0800

    v1.5.0-build.4535+sha.6406e3b

--- angular-cookies.js
+++ angular-cookies.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4530+sha.dc3013e
+ * @license AngularJS v1.5.0-build.4535+sha.6406e3b
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
